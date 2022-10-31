3c9546284afdab8d9f473cef98a40f1c6af3321e
Jonathan Elias Dawlabani <46453179+jdawlabani@users.noreply.github.com>
2022-10-31 09:24:49 -0400

    models finished

---
+++ flask_app/models/exercise.py
@@ -0,0 +1,72 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+from flask import flash
+import re
+
+#regex to check URL validation
+regex = ("((http|https)://)(www.)?" +
+            "[a-zA-Z0-9@:%._\\+~#?&//=]" +
+            "{2,256}\\.[a-z]" +
+            "{2,6}\\b([-a-zA-Z0-9@:%" +
+            "._\\+~#?&//=]*)")
+
+URL_REGEX = re.compile(regex)
+
+class Exercise:
+    db = 'meFit'
+    def __init__(self, data):
+        self.id = data['id']
+        self.name = data['name']
+        self.type = data['type']
+        self.video = data['video']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+        self.workouts = []
+
+    @classmethod
+    def create(cls, data):
+        query = "INSERT INTO exercises (name, type, video) VALUES (%(name)s, %(type)s, %(video)s)"
+        return connectToMySQL(cls.db).query_db(query, data)
+
+    @classmethod
+    def get_all(cls,data):
+        query = "SELECT * FROM exercises;"
+        results = connectToMySQL(cls.db).query_db(query)
+        exercise_list= []
+        for dict in results:
+            exercise_list.append(cls(dict))
+        return exercise_list
+
+    @classmethod
+    def update_by_id(cls, data):
+        query = "UPDATE exercises SET name = %(name)s, type = %(type)s, video = %(video)s WHERE id = %(id)s"
+        results = connectToMySQL(cls.db).query_db(query, data)
+        return
+    
+    # @classmethod
+    # def get_all_exercises_by_workout(cls,data):
+
+    @classmethod
+    def delete(cls, data):
+        query = "DELETE FROM exercises WHERE id = %(id)s;"
+        connectToMySQL(cls.db).query_db(query, data)
+        return
+
+    @staticmethod
+    def is_valid(exercise_data):
+        is_valid = False
+        all_exercises = Exercise.get_all()
+        all_types = ['Lower body', 'Push', 'Pull']
+        for type in all_types:
+            if type == exercise_data['type']:
+                is_valid = True
+        if len(exercise_data['name']) <= 2:
+            flash('Exercise name must be at least 2 characters long.')
+            is_valid = False
+        if not URL_REGEX.match(exercise_data['video']):
+            flash('Please enter a valid video URL.')
+            is_valid = False
+        for exercise in all_exercises:
+            if exercise.name == exercise_data['name']:
+                flash("Exercise name must be unique")
+                is_valid = False
+        return is_valid
--- flask_app/models/workout.py
+++ flask_app/models/workout.py
@@ -1,5 +1,5 @@
 from flask_app.config.mysqlconnection import connectToMySQL
-
+from flask import flash
 
 class Workout:
     db = 'meFit'
@@ -17,6 +17,45 @@ class Workout:
         return connectToMySQL(cls.db).query_db(query, data)
 
     @classmethod
+    def get_all(cls,data):
+        query = "SELECT * FROM workouts;"
+        results = connectToMySQL(cls.db).query_db(query)
+        workout_list= []
+        for dict in results:
+            workout_list.append(cls(dict))
+        return workout_list
+
+    @classmethod
     def get_all_workouts_by_user(cls, data):
         query = "SELECT * FROM workouts WHERE user_id = %(user_id)s"
+        results = connectToMySQL(cls.db).query_db(query)
+        workout_list= []
+        for dict in results:
+            workout_list.append(cls(dict))
+        return workout_list
 
+    @classmethod
+    def update_by_id(cls, data):
+        query = "UPDATE exercises SET name = %(name)s, type = %(type)s, video = %(video)s WHERE id = %(id)s"
+        results = connectToMySQL(cls.db).query_db(query, data)
+        return
+
+    #Still needs completion
+    @classmethod
+    def get_one_with_exercises(cls, data):
+        query = "SELECT * FROM workouts LEFT JOIN "
+        results = connectToMySQL(cls.db).query_db(query)
+        return results
+
+    @staticmethod
+    def is_valid(workout_data):
+        is_valid = True
+        all_workouts = Workout.get_all()
+        if len(workout_data['name']) <= 2:
+            flash('Workout name must be at least 2 characters long.')
+            is_valid = False
+        for workout in all_workouts:
+            if workout.name == workout_data['name']:
+                flash("Workout name must be unique")
+                is_valid = False
+        return is_valid
